fbbccc4c986f602edf2fda3c9e0f11322ef419d4
Juan Camilo <juancamilo@Juans-MacBook-Pro.local>
2018-06-07 15:55:17 -0500

    TRANS

--- js/transition.js
+++ js/transition.js
@@ -6,4 +6,17 @@ $("nav a").on("click", function(event){
 	
 	window.history.pushState(null, null, href);
 	
+	$.ajax({
+		url: href,
+		success:function(data){
+			$(".main").fadeOut(250, function(){
+				const newPage= $(data).filter(".about-main").html();
+				
+				$("section").html(newPage);
+				
+				$(".about-main").fadeIn(250);
+			});
+		}
+	});
+	
 })
